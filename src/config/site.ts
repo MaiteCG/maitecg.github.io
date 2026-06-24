e9fb8915fd58184b9f011f1c68836262f3124e99
MaiteCG <maity_winky@yahoo.es>
2026-06-24 16:47:18 +0100

    Second commit; Render publications from CSV

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+    name: "Maité Crespo García",
+    shortName: "M. Crespo-García",
+    initials: "MCG",
+
+    role: "Postdoctoral Researcher in Cognitive Neuroscience",
+    institution: "MRC Cognition and Brain Sciences Unit, University of Cambridge",
+    email: "crespogarciam@gmail.com",
+
+    links: {
+        github: "https://github.com/MaiteCG/",
+        scholar: "https://scholar.google.com/citations?hl=en&user=W97w76oAAAAJ&view_op=list_works&sortby=pubdate",
+        orcid: "https://orcid.org/0000-0002-1170-0382",
+        linkedin: "https://www.linkedin.com/in/maité-crespo-garcía-1b8144ab/",
+        researchgate: "https://www.researchgate.net/profile/Maite-Crespo-Garcia/research",
+        instagram: "https://www.instagram.com/maitinka/",
+      },
+  };
